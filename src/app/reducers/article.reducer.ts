13d149be815726f8404d862818c0bd7c67a60765
Jacob Sipple <sipple3@gmail.com>
2019-04-20 13:02:02 -0400

    removed initialstate in reducer

--- src/app/reducers/article.reducer.ts
+++ src/app/reducers/article.reducer.ts
@@ -1,12 +1,6 @@
 import { Action } from '@ngrx/store'
 import { Articles } from '../models/articles.modal'
 import * as ArticleActions from '../actions/actricles.actions'
-
-const initialState: Articles = {
- title: 'hello',
- link: 'http://localhost:3000',
- desc: 'hey'
-}
 // if you want initial to be loaded after Articles[] = [initialState]
 // need to initlalize as an empty array or it throws an error about state not being an array
 export function reducer(state: Articles[] = [], action: ArticleActions.Actions) {
